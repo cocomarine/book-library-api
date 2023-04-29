cf4a672398141e48e521fee45c70905112cefbdc
HJ Kang <hkang07@gmail.com>
2023-04-30 00:33:24 +0100

    API documentation in progress

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 
 const readerRouter = require('./routes/reader');
 const bookRouter = require('./routes/book');
@@ -7,11 +8,34 @@ const genreRouter = require('./routes/genre');
 
 const app = express();
 
+// const options = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'Book Library APIs',
+//       version: '1.0.0',
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:4000",
+//       },
+//     ],
+//   },
+//   apis: ['./routes/*.js', './controllers/*.js'], 
+// }
+
+const swaggerDocument = require('./swagger.json');
+
 app.use(express.json());
 
 app.use('/readers', readerRouter);
 app.use('/books', bookRouter);
 app.use('/authors', authorRouter);
 app.use('/genres', genreRouter);
+app.use(
+  '/api-docs', 
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument, { explorer: true })
+);
 
 module.exports = app;
